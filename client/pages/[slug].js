822bd940726d995a6cea38838ec407b407b11b96
Anh Vu <vu.haianh291@gmail.com>
2021-03-09 11:39:33 +0200

    Dynamic page

--- client/pages/[slug].js
+++ client/pages/[slug].js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
+import {useRouter} from 'next/router'
 import useSWR from 'swr'
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
-export default function Home() {
-    const {data, error} = useSWR('/api/pages/about', fetcher)
+export default function SlugPage() {
+    const router = useRouter()
+    const {slug} = router.query
+    const {data, error} = useSWR(`/api/pages/${slug}`, fetcher)
     if (error) return <div>error...</div>
     if (!data) return <div>loading...</div>
     return (
